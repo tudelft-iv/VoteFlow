838fd438160d4fab70990b076d00b21a0e693913
Shiming94 <shimingwang@c-001-007-011.client.tudelft.eduvpn.nl>
2024-10-22 16:27:08 +0200

    updated the slurm file for daic

--- slurm_scripts/daic/slurm_daic_sf_voxel_model.sh
+++ slurm_scripts/daic/slurm_daic_sf_voxel_model.sh
@@ -9,10 +9,10 @@
 #SBATCH --reservation=cvpr-cor
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=128G
-#SBATCH --gpus-per-task=1
+#SBATCH --gpus-per-task=8
 
 #SBATCH --mail-type=END
 #SBATCH --output=slurm_logs/slurm_sf_voxel_model_with_supervised_loss_%j.out
@@ -20,6 +20,7 @@
 
 module load cuda/11.8
 module load devtoolset/11
+module load miniconda/3.9
 
 export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
@@ -29,6 +30,6 @@ previous=$(nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,ma
 
 nvidia-smi
 
-srun python train.py model=sf_voxel_model lr=2e-5 epochs=20 batch_size=8 loss_fn=ff3dLoss wandb_mode=online exp_note="with_ff3dLoss"
+srun python train.py model=sf_voxel_model lr=2e-5 epochs=20 batch_size=2 loss_fn=ff3dLoss exp_note="with_ff3dLoss" wandb_mode="online" gpus="auto"
 
 nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
